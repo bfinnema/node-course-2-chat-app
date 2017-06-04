c3da259014288689a59799dbca81a7fca9eb51de
bfinnema <bfinnema@cisco.com>
2017-06-04 16:33:53 +0200

    Add form for messages adn show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user just joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage:', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('Dette kommer fra serveren.');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
